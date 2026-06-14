3105fdbe170d810897846b7e3f8a55deaf571611
hjm259775-a11y <hjm259775@gmail.com>
2026-06-14 19:26:14 +0800

    6.14

--- demo2/main.cpp
+++ demo2/main.cpp
@@ -1,2 +1,14 @@
-#include<iostream>﻿
-class 
+﻿#include<stdio.h>
+#include<unistd.h>
+
+int main()
+{
+    for (int i = 0; i <= 100;i++)
+    {
+        printf("\r[%3d%%]",i);
+        fflush(stdout);
+        usleep(20000);
+    }
+
+    return 0;
+}
