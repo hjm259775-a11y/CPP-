3729afd3487fa0cb373cba305a64cd8f0959d371
hjm259775-a11y <hjm259775@gmail.com>
2026-06-20 23:26:14 +0800

    6.20

--- CentralCache/CentralCache.h
+++ CentralCache/CentralCache.h
@@ -1,5 +1,5 @@
 ﻿#include<mutex>
-
+#include"SizeClass.h"
 
 
 
@@ -10,22 +10,54 @@ public:
     {
         for (int i = 0; i < 200;i++)
         {
-            _freelist[0] = nullptr;
+            _freelist[i] = nullptr;
         }
     }
 
-    void* FetchRange(int size,int num,int actualnum)
+    void* FetchRange(int size,int n,int actualnum)
     {
-        
+        int slot = Index(size);
+        mtx.lock();
+
+        void *head = _freelist[slot];
+        void *cur = head;
+        actualnum = 0;
+
+        while(cur!=nullptr && actualnum<n)
+        {
+            cur = *(void **)cur;
+            actualnum++;
+        }
+
+        if(actualnum>0)
+        {
+            _freelist[slot] = cur;
+        }
+        else
+        {
+            head = nullptr;
+        }
+
+        mtx.unlock();
+        return head;
     }
-    void* ReleaseRange(void* start,size_t size,size_t num)
+    void ReleaseRange(void* start,size_t size,size_t num)
     {
+        int slot = Index(size);
+        mtx.lock();
 
+        void *cur = start;
+        for (int i = 0; i < num;i++)
+        {
+            void *next = *(void **)cur;
+            *(void **)cur = _freelist[slot];
+            _freelist[slot] = cur;
+            cur = next;
+        }
+        mtx.unlock();
     }
-
-
 
 private:
     void *_freelist[200];
-
+    std::mutex mtx;
 };
--- CentralCache/main.cpp
+++ CentralCache/main.cpp
@@ -1,1 +1,26 @@
-﻿
+﻿#include<iostream>
+#include"CentralCache.h"
+
+
+int main()
+{
+    void *a = malloc(24);
+    void *b = malloc(24);
+    void *c = malloc(24);
+    *(void **)a = b;
+    *(void **)b = c;
+    *(void **)c = nullptr;
+
+    CentralCache xgz;
+    xgz.ReleaseRange(a, 24, 3);
+
+    void *head = xgz.FetchRange(24, 2, 0);
+    void *second = *(void **)head;
+
+    if(head==c && second==b)
+    {
+        std::cout << "好" << std::endl;
+    }
+
+    return 0;
+}
