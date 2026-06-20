b93162b9728c8976cf51e41aa29c713750f7fbdc
hjm259775-a11y <hjm259775@gmail.com>
2026-06-20 21:11:47 +0800

    6.20

---
+++ CentralCache/CentralCache.h
@@ -0,0 +1,31 @@
+﻿#include<mutex>
+
+
+
+
+class CentralCache
+{
+public:
+    CentralCache()
+    {
+        for (int i = 0; i < 200;i++)
+        {
+            _freelist[0] = nullptr;
+        }
+    }
+
+    void* FetchRange(int size,int num,int actualnum)
+    {
+        
+    }
+    void* ReleaseRange(void* start,size_t size,size_t num)
+    {
+
+    }
+
+
+
+private:
+    void *_freelist[200];
+
+};
---
+++ CentralCache/SizeClass.h
@@ -0,0 +1,38 @@
+﻿#include<cstddef>
+
+
+
+size_t RoundUp(size_t n)
+{
+    if(n<=128)
+    {
+        return ((n + 7) / 8) * 8;
+    }
+    else if(n<=1024)
+    {
+        return ((n + 15) / 16) * 16;
+    }
+    else if(n<=4096)
+    {
+        return ((n + 31) / 32) * 32;
+    }
+}
+
+
+
+
+size_t Index(size_t n)
+{
+    if(n<=128)
+    {
+        return (n + 7) / 8;
+    }
+    else if(n<=1024)
+    {
+        return 16 + (n + 15) / 16;
+    }
+    else if(n<=4096)
+    {
+        return 16 + 56 + (n + 31) / 32;
+    }
+}
