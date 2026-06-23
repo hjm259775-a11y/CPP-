1e32568e6017bb91b2ddf450fef327a56d82e69d
hjm259775-a11y <hjm259775@gmail.com>
2026-06-23 22:23:40 +0800

    6.23

---
+++ PageCache/Span.h
@@ -0,0 +1,67 @@
+﻿#include<cstddef>
+
+struct span
+{
+    void *start;
+    size_t size;
+    bool _is;
+    span *next;
+    span *prev;
+};
+
+
+
+class SpanList
+{
+public:
+    SpanList()
+    {
+        head.next = &head;
+        head.prev = &head;
+    }
+
+    void pushfront(span* n)
+    {
+        n->next = head.next;
+        head.next->prev = n;
+        head.next = n;
+        n->prev = &head;
+    }
+
+    span* popfront()
+    {
+        span *der = head.next;
+        span *derr = der->next;
+        derr->prev = &head;
+        head.next = derr;
+        return der;
+    }
+
+    void* remove(span* n)
+    {
+        span *r = n->next;
+        span *l = n->prev;
+        l->next = r;
+        r->prev = l;
+        n->next = nullptr;
+        n->prev = nullptr;
+
+        void *h = n->start;
+        return h;
+    }
+
+    bool Empty()
+    {
+        if(head.next == &head &&head.prev == &head)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+private:
+    span head;
+};
