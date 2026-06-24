284a40778995f61e2702dea9b36c09d169cf2f15
hjm259775-a11y <hjm259775@gmail.com>
2026-06-24 22:33:27 +0800

    6.24

--- CentralCache/CentralCache.h
+++ CentralCache/CentralCache.h
@@ -41,6 +41,9 @@ public:
         mtx.unlock();
         return head;
     }
+    //从链表头拿n个
+
+
     void ReleaseRange(void* start,size_t size,size_t num)
     {
         int slot = Index(size);
@@ -56,6 +59,7 @@ public:
         }
         mtx.unlock();
     }
+    //把一串头插回去
 
 private:
     void *_freelist[200];
