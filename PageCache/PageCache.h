f98d2592524d2f39d77f58639df212dc2ea1fb6c
hjm259775-a11y <hjm259775@gmail.com>
2026-06-24 21:59:11 +0800

    6.24

--- PageCache/PageCache.h
+++ PageCache/PageCache.h
@@ -1,7 +1,51 @@
 ﻿#include"Span.h"
+#include<mutex>
 
 
-span Newspan(size_t k)
+class PageCache
 {
+public:
+    PageCache(){}
+    span *Newspan(size_t k)
+    {
+        if(freelist[k].Empty())
+        {
+            for (int i = k + 1; i < 129; i++)
+            {
+                if(freelist[i].Empty()==false)
+                {
+                    span *big = freelist[i].popfront();
+
+                    span *gei = new span;
+                    gei->start = big->start;
+                    gei->size = k;
+                    gei->_is = true;
+                    //扔给用户的
+
+                    big->start = big->start + k * 8192;
+                    big->size = i - k;
+                    big->_is = false;
+                    //返回链表的
+
+                    freelist[i - k].pushfront(big);
+
+                    return gei;
+                }
+            }
+            void* 
+        }
+        else
+        {
+            return freelist[k].popfront();
+        }
+    }
+    void DeleteSpan(span *n)
+    {
+        
+    }
     
-}
+
+private:
+    SpanList freelist[129];
+    std::mutex mtx;
+};
