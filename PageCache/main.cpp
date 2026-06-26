61c3481bf2cdf9f9c9af0ac7bc5e32e6474975bf
hjm259775-a11y <hjm259775@gmail.com>
2026-06-26 23:23:50 +0800

    6.26

--- PageCache/main.cpp
+++ PageCache/main.cpp
@@ -1,1 +1,15 @@
 ﻿#include<iostream>
+#include"PageCache.h"
+
+int main()
+{
+    PageCache pc;
+    span *s1 = pc.Newspan(1);//要一页
+    std::cout << s1->start << " " << s1->size << std::endl;
+
+    span *s2 = pc.Newspan(2);//要两页
+    std::cout << s2->start << " " << s2->size << std::endl;
+
+    
+    return 0;
+}
