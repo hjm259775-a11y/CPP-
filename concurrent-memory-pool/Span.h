4a9087dbc647b8b62a6554976755ba70557d85b0
hjm259775-a11y <hjm259775@gmail.com>
2026-06-28 23:03:53 +0800

    6.28

---
+++ concurrent-memory-pool/Span.h
@@ -0,0 +1,45 @@
+﻿#include"SizeClass.h"
+#include<vector>
+
+struct span
+{
+    void *start;
+    size_t size;
+    span *next;
+    span *prev;
+};
+
+
+class PageCache
+{
+public:
+    PageCache()
+    {
+        head.next = &head;
+        head.prev = &head;
+    }
+
+    void Pushspan(span* n)
+    {
+        n->next = head.next;
+        head.next->prev = n;
+        head.next = n;
+        n->prev = &head;
+    }
+    span* Popspan()
+    {
+        if(head.next==&head)
+        {
+            return nullptr;
+        }
+        span *h = head.next;
+        head.next = head.next->next;
+        head.next->prev = &head;
+        h->next = nullptr;
+        h->prev = nullptr;
+        return h;
+    }
+
+private:
+    span head;
+};
